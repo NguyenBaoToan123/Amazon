d3b9e0a8f3d124f5f3525df824fdaa4de8d5bc2c
BaoToan <nguyenbaotoan22102006@gmail.com>
2025-03-22 12:58:51 -0500

    new

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,20 @@ import {renderPaymentSummary} from "./checkout/paymentSummary.js"
 import { loadProducts } from "../data/products.js";
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js';
+
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve() 
+    })
+    //resolve let us go to the second step
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+/*
 loadProducts(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
